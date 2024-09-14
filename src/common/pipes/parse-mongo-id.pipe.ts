108c8ebab40d4d903a800ae67b384434b8a169d2
Yasid p <yapc6465@gmail.com>
2024-09-14 15:37:16 -0500

    API REST POKEDEX

---
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -0,0 +1,16 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+  
+  transform(value: string, metadata: ArgumentMetadata) {
+
+    if(!isValidObjectId(value)){
+      throw new BadRequestException('Invalid MongoDB ID');
+    }
+
+    return value;
+  }
+
+}
